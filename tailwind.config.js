6d4149695167f2cecb33a184b6e89f1b1fc52b95
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-02-04 23:49:54 +0530

    Fix sizes and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,45 @@
 module.exports = {
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: [
+        "MarkPro",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: [
+        "Copernicus",
+        "Georgia",
+        "Cambria",
+        '"Times New Roman"',
+        "Times",
+        "serif",
+      ],
+      mono: [
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        '"Liberation Mono"',
+        '"Courier New"',
+        "monospace",
+      ],
+    },
+    extend: {
+      fontSize: {
+        "7xl": "6rem",
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
