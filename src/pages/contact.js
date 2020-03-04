e5f40d539951f20a9dc0020eb81035d4e0b9859e
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-03-04 15:32:57 +0530

    Hook up contact page with data from WP

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,56 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../layouts/index"
 import HeroTitle from "../components/HeroTitle"
 import ContactForm from "../components/ContactForm"
 import ContactDetails from "../components/ContactDetails"
 
-export default () => (
-  <Layout>
-    <HeroTitle>Get in touch.</HeroTitle>
-    <div className="flex text-gray-400">
-      <ContactForm />
-    </div>
-  </Layout>
-)
+export default ({ data }) => {
+  const {
+    WP: { page },
+  } = data
+  return (
+    <Layout>
+      <HeroTitle>{page.contact.hero}</HeroTitle>
+      <div className="flex text-gray-400">
+        <ContactForm />
+        {page.contact.contactasideblock.map(
+          ({ title, description, hasLink, linktext, link }) => (
+            <div className="w-2/6 ml-auto flex flex-col">
               <ContactDetails
                 title={title}
                 description={description}
                 link={link}
                 linkText={linktext}
               />
+            </div>
+          )
+        )}
+        <ContactDetails />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    WP {
+      page(id: "cGFnZTozMQ==") {
+        contact {
+          hero
+          contactform {
+            formdescription
+          }
+          contactasideblock {
+            title
+            description
+            haslink
+            linktext
+            link
+          }
+        }
+      }
+    }
+  }
+`
