9295fe6fbf72e3569fcca6daa005e95e986a3e54
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-03-04 18:41:27 +0530

    Open phone numbers and email IDs on the contact page in the correct applications when clicked

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,16 +1,27 @@
 import React from "react"
 
-export default ({ details }) =>
+export default ({ details }) => (
   <div className="w-2/6 ml-auto">
     {details.map(({ title, description, link }) => (
-    <div>
-      <span className="text-xs text-white block border-b border-gray-500 font-semibold py-2 font-sans">
-        {title}
-      </span>
-      <p className="py-2 text-xs font-serif">{description}</p>
-      <a className="text-xs text-bittersweet font-sans" href={link.href}>
-        {link.text}
-      </a>
-    </div>
-  ))}
+      <div>
+        <span className="text-xs text-white block border-b border-gray-500 font-semibold py-2 font-sans">
+          {title}
+        </span>
+        {link.typeoflink === "Email" ? (
+          <p className="py-2 text-xs font-serif">
+            <a href={`mailto:${description}`}>{description}</a>
+          </p>
+        ) : link.typeoflink === "Phone Number" ? (
+          <p className="py-2 text-xs font-serif">
+            <a href={`tel:${description}`}>{description}</a>
+          </p>
+        ) : (
+          <p className="py-2 text-xs font-serif">{description}</p>
+        )}
+        <a className="text-xs text-bittersweet font-sans" href={link.href}>
+          {link.text}
+        </a>
+      </div>
+    ))}
   </div>
+)
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -40,7 +40,7 @@ export const query = graphql`
             link {
               text
               href
-              isexternal
+              typeoflink
             }
           }
         }
