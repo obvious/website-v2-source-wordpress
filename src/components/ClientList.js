e98cc056f2096f0afd2c89b74c1f9195fa63d227
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-03-05 11:15:27 +0530

    Refactor ClientList to use query data, display all blocks internall

--- src/components/ClientList.js
+++ src/components/ClientList.js
@@ -1,18 +1,18 @@
 import React from "react"
 
-export default ({ clients, category }) => {
-  return (
-    <div className="w-1/3 my-4 mr-4">
-      <h2 className="font-semibold text-xs text-white border-b border-gray-500 py-2">
-        {category}
-      </h2>
-      <ul className="mt-4">
-        {clients.map((client, index) => (
-          <li key={index} className="font-semibold text-xs py-1">
-            {client}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+export default ({ listOfClientListBlocks }) => (
+  <div className="container py-2 text-gray-500 flex flex-wrap items-start justify-between font-sans">
+    {listOfClientListBlocks.map(({ clientlistblock }) => (
+      <div className="w-1/3 my-4 mr-4">
+        <h2 className="font-semibold text-xs text-white border-b border-gray-500 py-2">
+          {clientlistblock.title}
+        </h2>
+        <ul className="mt-4">
+          {clientlistblock.namelist.map(({ name }) => (
+            <li className="font-semibold text-xs py-1">{name}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>
+)
--- src/pages/clients.js
+++ src/pages/clients.js
@@ -6,49 +6,18 @@ import HeroTitle from "../components/HeroTitle"
 import ClientList from "../components/ClientList"
 import FeaturedClient from "../components/FeaturedClient"
 
-  <Layout>
-    <div className="container py-2 text-gray-500 flex flex-wrap items-start justify-between font-sans">
-      <ClientList
-        category="Payments"
-        clients={[
-          `Slicepay`,
-          `Zoto`,
-          `Fisdom`,
-          `Citruspay`,
-          `Payzippy`,
-          `Paisool`,
-        ]}
-      />
-      <ClientList
-        category="Logistics"
-        clients={[`Dunzo`, `Swiggy`, `Zomato`, `Gojek`]}
-      />
-      <ClientList
-        category="Not-For-Profit"
-        clients={[
-          `Resolve to Save Lives`,
-          `Pratham Books`,
-          `YourStory`,
-          `Ashoka Foundation`,
-        ]}
-      />
-      <ClientList
-        category="Education"
-        clients={[`Gradeup`, `IMAX`, `Loco`, `Wikipedia`]}
-      />
-      <ClientList
-        category="Ecommerce"
-        clients={[`Flipkart`, `2Gud`, `Flyte`, `OLX`]}
-      />
-    </div>
-  </Layout>
-)
 export default ({ data }) => {
   const {
     WP: { page },
   } = data
+  return (
+    <Layout>
       <HeroTitle>{page.client.hero}</HeroTitle>
       <FeaturedClient featuredClientBlock={page.client.featuredclientblock} />
+      <ClientList listOfClientListBlocks={page.client.listofclientlistblocks} />
+    </Layout>
+  )
+}
 
 export const query = graphql`
   {
