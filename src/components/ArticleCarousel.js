c219bb27b5efac898d54b7ef152c5583ac1792ab
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-03-05 16:35:16 +0530

    Add query to get all articles

--- src/components/ArticleCarousel.js
+++ src/components/ArticleCarousel.js
@@ -1,33 +1,48 @@
 import React from "react"
+import { graphql } from "gatsby"
+
 import { Link } from "gatsby"
+
 
 export function ArticleCarouselCard({ title, date }) {
   return (
     <Link className="rounded bg-hello w-2/5 mr-4 p-4 flex flex-col justify-between h-40">
       <h4 className="text-white font-sans font-semibold text-lg">{title}</h4>
-      <span className="text-hello font-sans uppercase tracking-wide font-semibold text-sm">
+      <span className="text-world font-sans uppercase tracking-wide font-semibold text-sm">
         {date}
       </span>
     </Link>
   )
 }
 
-export default () => {
+export default ({ data }) => {
+  const {
+    WP: { page },
+  } = data
+
+  //TODO: function to order articles and filter for the lastest fifteen 
+
   return (
     <div className="flex py-4">
       <ArticleCarouselCard
         title="A day in the life of a Simple engineer"
         date="January 8, 2019"
       />
-      <ArticleCarouselCard title="Rahul is cool" date="January 8, 2019" />
-      <ArticleCarouselCard
-        title="Pratul likes watches"
-        date="January 8, 2019"
-      />
-      <ArticleCarouselCard
-        title="I'll buy you cake if you can make Dhruv laugh out loud"
-        date="January 8, 2019"
-      />
     </div>
   )
 }
+
+export const query = graphql`
+    {
+        WP {
+            articles {
+                edges {
+                    node {
+                        id
+                        title
+                        date
+                    }
+                }
+            }
+        }
+    }`
