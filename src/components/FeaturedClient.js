9894ec3483ff191f6da665d940678ca60d0723f0
Tanvi Bhakta <tanvi@obvious.in>
2020-03-07 17:46:59 +0530

    feat: Hook up clients page with WP (#4)

--- src/components/FeaturedClient.js
+++ src/components/FeaturedClient.js
@@ -1,16 +1,18 @@
 import React from "react"
-// import { Link } from "gatsby"
+import Layout from "../layouts"
 
-export default ({ client, blurb, caseStudy }) => {
-  return (
-    <div className="w-1/3 my-4 mr-4">
-      <h2 className="font-semibold text-xl text-white font-sans">{client}</h2>
-      <p className="py-2 font-serif">{blurb}</p>
-      {caseStudy && (
-        <a className="text-xs text-bittersweet font-sans" href={caseStudy}>
-          Read Case Study
-        </a>
-      )}
-    </div>
-  )
-}
+export default ({ featuredClientBlock }) => (
+  <div className="flex text-gray-400 flex-wrap justify-between">
+    {featuredClientBlock.map(({ client, description, link }) => (
+      <div className="w-1/3 my-4 mr-4">
+        <h2 className="font-semibold text-xl text-white font-sans">{client}</h2>
+        <p className="py-2 font-serif">{description}</p>
+        {link && (
+          <a className="text-xs text-bittersweet font-sans" href={link.href}>
+            {link.text}
+          </a>
+        )}
+      </div>
+    ))}
+  </div>
+)
--- src/pages/clients.js
+++ src/pages/clients.js
@@ -1,81 +1,49 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../layouts/index"
 import HeroTitle from "../components/HeroTitle"
 import ClientList from "../components/ClientList"
 import FeaturedClient from "../components/FeaturedClient"
 
-export default () => (
-  <Layout>
-    <HeroTitle>
-      A blurb about our clients and we transform ideas into household names.
-    </HeroTitle>
-    <div className="flex text-gray-400 flex-wrap justify-between">
-      <FeaturedClient
-        client="Quintype"
-        blurb="Reengineering India’s leading news org’s web-publishing platform to work
-        2x faster"
-        caseStudy="https://obvious.in/case-study/quintype/"
-      />
-      <FeaturedClient
-        client="Dunzo"
-        blurb="Streamlining engineering to create product scalability for Dunzo’s
-          hyperlocal market"
-        caseStudy="https://obvious.in/case-study/dunzo/"
-      />
-      <FeaturedClient
-        client="Gojek"
-        blurb="Redesigning the driver partner app for Indonesia’s most loved
-          ride-sharing aggregator"
-        caseStudy="https://obvious.in/case-study/gojek/"
-      />
-      <FeaturedClient
-        client="Loco"
-        blurb="Redesigning Pocket Aces’ game app with 10M+ downloads that lets you
-          win real money"
-        caseStudy="https://obvious.in/case-study/loco/"
-      />
-      <FeaturedClient
-        client="2GUD"
-        blurb="Contributing ~$1B in revenue by product vision for Flipkart’s pivot
-          into refurbished products market"
-        caseStudy="https://obvious.in/case-study/2gud/"
-      />
-    </div>
-    <div className="container py-2 text-gray-500 flex flex-wrap items-start justify-between font-sans">
-      <ClientList
-        category="Payments"
-        clients={[
-          `Slicepay`,
-          `Zoto`,
-          `Fisdom`,
-          `Citruspay`,
-          `Payzippy`,
-          `Paisool`,
-        ]}
-      />
-      <ClientList
-        category="Logistics"
-        clients={[`Dunzo`, `Swiggy`, `Zomato`, `Gojek`]}
-      />
-      <ClientList
-        category="Not-For-Profit"
-        clients={[
-          `Resolve to Save Lives`,
-          `Pratham Books`,
-          `YourStory`,
-          `Ashoka Foundation`,
-        ]}
-      />
-      <ClientList
-        category="Education"
-        clients={[`Gradeup`, `IMAX`, `Loco`, `Wikipedia`]}
-      />
-      <ClientList
-        category="Ecommerce"
-        clients={[`Flipkart`, `2Gud`, `Flyte`, `OLX`]}
-      />
-    </div>
-  </Layout>
-)
+export default ({ data }) => {
+  const {
+    WP: { page },
+  } = data
+  return (
+    <Layout>
+      <HeroTitle>{page.client.hero}</HeroTitle>
+      <FeaturedClient featuredClientBlock={page.client.featuredclientblock} />
+      <ClientList listOfClientListBlocks={page.client.listofclientlistblocks} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    WP {
+      page(id: "cGFnZTo0Mg==") {
+        client {
+          hero
+          featuredclientblock {
+            client
+            description
+            link {
+              text
+              href
+              typeoflink
+            }
+          }
+          listofclientlistblocks {
+            clientlistblock {
+              title
+              namelist {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
