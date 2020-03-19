2c876eabfee38d17ec2060106a5e415f7c26cefd
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-03-19 22:50:16 +0530

    Add styles for title text, image caption

--- src/components/ArticleTitle.js
+++ src/components/ArticleTitle.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default ({ children }) => {
   return (
-    <h1 className="font-sans text-4xl font-semibold leading-tight my-4">
+    <h1 className="font-sans text-6xl font-bold text-gray-10 leading-tight my-4">
       {children}
     </h1>
   )
