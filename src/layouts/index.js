2b5c245a3e8cc798c2e4d481f916cad40c2fb820
Meet Bhatt <meetbhatt@obvious.in>
2020-04-10 02:21:10 +0530

    Fetch navigation from WP, combine mobile and web menus, UI fixes

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,34 +5,33 @@ import SiteHeader from "../components/SiteHeader"
 import SiteFooter from "../components/SiteFooter"
 
 export default ({ children, showFooter = false, className }) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggleIsOpen = () => setIsOpen(!isOpen)
+  
   return (
-    <div className={`flex flex-col justify-start antialiased bg-gray-10 min-h-screen ${className}`}>
-      <SiteHeader isOpen={isOpen} toggleIsOpen={toggleIsOpen} />
+    <div className={`flex flex-col justify-start antialiased bg-gray-10 pt-14 min-h-screen ${className}`}>
+      <SiteHeader />
       <main className="container mx-auto">
-        {isOpen && (
-          <div className="px-10 text-world flex justify-end text-right py-4 absolute z-10 bg-black w-screen border-world border-t">
-            <ul>
-              <Link className="px-2" activeClassName="text-white" to="/clients">
-                <li>Clients</li>
-              </Link>
-              <Link
-                className="px-2"
-                activeClassName="text-white"
-                to="/publications"
-              >
-                <li>Publications</li>
-              </Link>
-              <a className="px-2" href="https://playbook.obvious.in/">
-                <li>Playbook</li>
-              </a>
-              <Link className="px-2" activeClassName="text-white" to="/contact">
-                <li>Contact</li>
-              </Link>
-            </ul>
-          </div>
-        )}
+        {/*{isOpen && (*/}
+        {/*  <div className="px-10 text-world flex justify-end text-right py-4 absolute z-10 bg-black w-screen border-world border-t">*/}
+        {/*    <ul>*/}
+        {/*      <Link className="px-2" activeClassName="text-white" to="/clients">*/}
+        {/*        <li>Clients</li>*/}
+        {/*      </Link>*/}
+        {/*      <Link*/}
+        {/*        className="px-2"*/}
+        {/*        activeClassName="text-white"*/}
+        {/*        to="/publications"*/}
+        {/*      >*/}
+        {/*        <li>Publications</li>*/}
+        {/*      </Link>*/}
+        {/*      <a className="px-2" href="https://playbook.obvious.in/">*/}
+        {/*        <li>Playbook</li>*/}
+        {/*      </a>*/}
+        {/*      <Link className="px-2" activeClassName="text-white" to="/contact">*/}
+        {/*        <li>Contact</li>*/}
+        {/*      </Link>*/}
+        {/*    </ul>*/}
+        {/*  </div>*/}
+        {/*)}*/}
         <div className="px-10 sm:px-20 sm:py-10">{children}</div>
       </main>
       {showFooter && <SiteFooter />}
