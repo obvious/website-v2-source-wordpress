7efdcdf439aebd5f0b7a2cf0727b21efb493b0f8
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-04-21 12:36:28 +0530

    Add heading styles to the tags themselves so that they can apply to data coming in from Gutenberg

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "../styles/Heading.css"
 
 /**
  * A note on text-{color}: The specificity of text-color is determined not by
@@ -19,14 +20,15 @@ export const Heading = ({ content, children, tag, type, className }) => {
     case "h1":
       return (
         <h1
-          className={`${baseClass} ${className} text-8xl font-bold lg:text-9xl leading-none`}
+          className={`${baseClass} ${className} text-8xl lg:text-9xl font-bold leading-none`}
         >
           {children}
         </h1>
       )
     case "h2":
       return (
-        //TODO: It is medium on FeaturedClient, semibold on ClientList, and normal on the design system. Can we standardise this/put this in the typography?
+        //TODO: It is medium on FeaturedClient, semibold on ClientList, and normal on the design system.
+        // Can we standardise this/put this in the typography?
         <h2
           className={`${baseClass} ${className} text-7xl font-medium lg:text-8xl leading-tight lg:leading-none`}
         >
@@ -66,8 +68,6 @@ export const Heading = ({ content, children, tag, type, className }) => {
         </h6>
       )
 
-    //TODO: Add styles to target html tags specifically so that they look like this and make sense.
-    //TODO: Those tags should not override the styles mentioned above, and should be overridable by other styles.
     //TODO: Also add hyperlink styles to this one
 
     default:
