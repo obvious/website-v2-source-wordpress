2f34f2f6c823cc0766f4f491fcaa2f497ad52a56
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-04-28 20:03:33 +0530

    Style citations in blockquotes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,10 @@
 import React from "react"
+import "../styles/Quote.css"
 
 //TODO: Display separators above and below quote on mobile screens
 export default ({ children, author, bio }) => {
   return (
-    <div className="my-4 lg:my-10">
+    <div className="quote-container my-4 lg:my-10">
       <p className="font-serif font-extrabold text-xl text-grey-10 italic leading-relaxed" dangerouslySetInnerHTML={{__html: children}} />
       { (author || bio) && <div>
         {author && <span className="text-xs text-black font-sans font-semibold">
