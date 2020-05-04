f8480ff35dfb0edd6004867f54ddbecb2b0a97d8
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-05-04 17:26:13 +0530

    Article Card creates appropriate link based on article or case study

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -7,7 +7,7 @@ import { Byline } from "./Byline"
 export default ({ iscasestudy, title, excerpt, date, author, slug }) => {
   let datePublished = new Date(date).toLocaleDateString(undefined, {month: 'long', day: 'numeric', year: 'numeric'});
   return (
-    <Link to={`articles/${slug}`}>
+    <Link to={iscasestudy ? `case-studies/${slug}` : `articles/${slug}`}>
       <div className="bg-white w-full p-6 mb-6 rounded-sm">
         {/*TODO: refactor once heading has been refactored to include tag-picking*/}
         <Heading className="text-gray-10 border-gray-300 border-b-2 pb-2" content={`<h4>${title}</h4>`} />
