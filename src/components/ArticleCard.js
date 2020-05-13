ee0a0cb14281597f04ae0956d61e03605c510948
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-05-13 14:25:14 +0530

    Converted Article Card

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -8,13 +8,14 @@ export default ({ iscasestudy, title, excerpt, date, author, slug }) => {
   let datePublished = new Date(date).toLocaleDateString(undefined, {month: 'long', day: 'numeric', year: 'numeric'});
   return (
     <Link to={iscasestudy ? `case-studies/${slug}` : `articles/${slug}`}>
-      <div className="bg-white w-full p-6 mb-6 rounded-sm">
+      <div className="bg-white w-full p-8 mb-8 rounded-sm">
         {/*TODO: refactor once heading has been refactored to include tag-picking*/}
-        <Heading className="text-gray-10 border-gray-300 border-b-2 pb-2" content={`<h4>${title}</h4>`} />
-        { iscasestudy || <Byline date={datePublished} author={author} className="pt-3" isArticleCard />}
+        <Heading className="text-gray-10 border-light/gray-50 border-b-2 pb-4 mb-4" content={`<h4>${title}</h4>`} />
+        { iscasestudy || <Byline date={datePublished} author={author} className="mb-7" isArticleCard />}
+        {/*Excerpt must always exist - see NUWEB-99*/}
         {excerpt && <BodyText
           type="body-medium"
-          className="text-light/gray-10 mt-4"
+          className="text-light/gray-10"
           content={excerpt}
         />}
       </div>
