b4619984ade6c92b1fd361e9d18fdf067ab38ba9
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-05-21 15:06:55 +0530

    Add figcaption to PreviewCompatibleImages

--- src/components/atoms/PreviewCompatibleImage.js
+++ src/components/atoms/PreviewCompatibleImage.js
@@ -1,36 +1,45 @@
 import GatsbyImage from "gatsby-image"
 import React from "react"
 import SVG from "react-inlinesvg"
+import "../../styles/Image.css"
 
 const PreviewCompatibleImage = ({ image, className }) => {
   const extensionRegex = /(?:\.([^.]+))?$/
   const src = image.sourceUrl ? image.sourceUrl : image.attributes.url
   const extension = extensionRegex.exec(src)[1]
+  //TODO: filter out gifs
   if (image.imageFile && image.imageFile.childImageSharp) {
     if (image.imageFile.childImageSharp.fixed)
       return (
-        <GatsbyImage
-          className={className}
-          fixed={image.imageFile.childImageSharp.fixed}
-          alt=""
-        />
+        <>
+          <GatsbyImage
+            className={className}
+            fixed={image.imageFile.childImageSharp.fixed}
+            alt=""
+          />
+          {image.attributes && image.attributes.caption && (
+            <figcaption>{image.attributes.caption}</figcaption>
+          )}
+        </>
       )
     else
       return (
-        <GatsbyImage
-          className={className}
-          fluid={image.imageFile.childImageSharp.fluid}
-          alt=""
-        />
+        <>
+          <GatsbyImage
+            className={className}
+            fluid={image.imageFile.childImageSharp.fluid}
+            alt=""
+          />
+          {image.attributes && image.attributes.caption && (
+            <figcaption>{image.attributes.caption}</figcaption>
+          )}
+        </>
       )
   } else {
     if (extension === "svg") {
       // TODO - change this to use SVGR
       return <SVG className={className || ""} src={image.imageFile.publicURL} />
-    } else
-      return (
-        <img className={className} src={src} alt="" />
-      )
+    } else return <img className={className} src={src} alt="" />
   }
 }
 
