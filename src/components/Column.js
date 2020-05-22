1195effa65a7a8eb59f4077130b9a75a307b16d9
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-05-22 07:59:31 +0530

    Update incorrect assumption about column number

--- src/components/Column.js
+++ src/components/Column.js
@@ -3,9 +3,6 @@ import { getColSpan } from "../utils/column-utils"
 import { assignComponent } from "../templates/article"
 
 export const Column = ({ block }) => {
-  // Another assumption this piece of code relies on, is that there will only
-  // be two columns! This should be implemented using the `columns` attribute
-  // on the WP_CORE_COLUMNS block
   const columnWidth = block.columnattributes.width
   const columnSize = getColSpan(columnWidth)
   return (
